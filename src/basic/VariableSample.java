4af0a345dc11f71a4cd1ac7a5ca5324cbeb3361c
nobu-kudo <vstand66@gmail.com>
2026-07-02 21:49:26 +0900

    VariableSampleを追加

---
+++ src/basic/VariableSample.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class VariableSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		String name = "Taro";
+		int age = 25;
+		double height = 170.5;
+		boolean student = true;
+		
+		System.out.println("名前" + name);
+		System.out.println("年齢" + age);
+		System.out.println("身長" + height);
+		System.out.println("学生" + student);
+	}
+
+}
