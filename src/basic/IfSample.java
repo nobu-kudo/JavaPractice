c2e569b1c1a4c4a022394e8c8a75ddfa2ee8b3b2
nobu-kudo <vstand66@gmail.com>
2026-07-02 22:01:44 +0900

    IfSampleを追加

---
+++ src/basic/IfSample.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class IfSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int score = 80;
+		
+		if(score >= 60) {
+			System.out.println("合格です");
+			
+	}else {
+		System.out.println("不合格です");
+	}
+
+}
+}
