1941f6e458e39aaea489033070854158bd4a787e
nobu-kudo <vstand66@gmail.com>
2026-07-03 23:13:29 +0900

    ForSampleを追加

---
+++ src/basic/ForSample.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class ForSample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		for(int i = 1; i <= 10; i++ ) {
+			System.out.println(i);
+		}
+	}
+
+}
