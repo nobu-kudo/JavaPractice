a0b3fab7f577349a611c144b93da97e880512e93
nobu-kudo <vstand66@gmail.com>
2026-07-05 01:20:15 +0900

    ArraySample.javaの追加

---
+++ src/basic/ArraySample.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class ArraySample {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int[] score = { 50, 80, 70, 90, 60};
+		System.out.println(score.length);
+		for(int i = 0; i < score.length; i++) {
+		System.out.println(score[i]);
+		}
+	}
+
+}
