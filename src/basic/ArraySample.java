30c6df38c26c9585a7245b8b329cdb005789839a
nobu-kudo <vstand66@gmail.com>
2026-07-05 17:46:19 +0900

    ArraySampleに最大値と最小値の処理の追加

--- src/basic/ArraySample.java
+++ src/basic/ArraySample.java
@@ -7,13 +7,31 @@ public class ArraySample {
 		int[] score = { 50, 80, 70, 90, 60};
 		int sum = 0;
 		int avg = 0;
-		System.out.println(score.length);
+		int max = score[0];
+		int min = score[0];
+		//要素の取り出し
+		System.out.println("要素数:" + score.length);
+		//合計の処理
 		for(int i = 0; i < score.length; i++) {
 			sum = sum + score[i];
 		}
+		//平均の処理
 			avg = sum / score.length;
-		System.out.println(sum);
-		System.out.println(avg);
+		//合計・平均の出力
+		System.out.println("合計:" + sum);
+		System.out.println("平均:" + avg);
+		//最大値の処理
+		for(int i = 0; i < score.length; i++) {
+			if(score[i] > max) {
+				max = score[i];
+			}
+		//最小値の処理
+				if(score[i] < min) {
+					min = score[i];
+				}
+		}
+		System.out.println("最大値:" + max);
+		System.out.println("最小値:" + min);
 	}
 
 }
