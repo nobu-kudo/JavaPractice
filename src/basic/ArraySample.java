c53fb83bda5642891112cc9f1cf69e3ce6424e12
nobu-kudo <vstand66@gmail.com>
2026-07-05 17:14:07 +0900

    ArraySampleに合計と平均の処理を追加

--- src/basic/ArraySample.java
+++ src/basic/ArraySample.java
@@ -5,10 +5,15 @@ public class ArraySample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		int[] score = { 50, 80, 70, 90, 60};
+		int sum = 0;
+		int avg = 0;
 		System.out.println(score.length);
 		for(int i = 0; i < score.length; i++) {
-		System.out.println(score[i]);
+			sum = sum + score[i];
 		}
+			avg = sum / score.length;
+		System.out.println(sum);
+		System.out.println(avg);
 	}
 
 }
