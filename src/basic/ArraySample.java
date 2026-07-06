277efc581135a5f702b029f8194c7fed5c11c72e
nobu-kudo <vstand66@gmail.com>
2026-07-06 22:29:18 +0900

    ArraySamplejavaの編集

--- src/basic/ArraySample.java
+++ src/basic/ArraySample.java
@@ -11,25 +11,25 @@ public class ArraySample {
 		int min = score[0];
 		//要素の取り出し
 		System.out.println("要素数:" + score.length);
-		//合計の処理
+		
 		for(int i = 0; i < score.length; i++) {
+			//合計の処理
 			sum = sum + score[i];
-		}
-		//平均の処理
+			//最大値の処理
+			if(score[i] > max) {
+				max = score[i];
+			//最小値の処理
+			if(score[i] < min) {
+				min = score[i];
+			//平均の処理
 			avg = sum / score.length;
+			}
+
+			}
+		}
 		//合計・平均の出力
 		System.out.println("合計:" + sum);
 		System.out.println("平均:" + avg);
-		//最大値の処理
-		for(int i = 0; i < score.length; i++) {
-			if(score[i] > max) {
-				max = score[i];
-			}
-		//最小値の処理
-				if(score[i] < min) {
-					min = score[i];
-				}
-		}
 		System.out.println("最大値:" + max);
 		System.out.println("最小値:" + min);
 	}
