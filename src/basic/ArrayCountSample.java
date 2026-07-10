4fe4608b57fa19f5c548da45512f4c9a514817c8
nobu-kudo <vstand66@gmail.com>
2026-07-10 19:53:04 +0900

    ArrayCountSample.java

--- src/basic/ArrayCountSample.java
+++ src/basic/ArrayCountSample.java
@@ -6,13 +6,16 @@ public class ArrayCountSample {
 		// TODO 自動生成されたメソッド・スタブ
 		int[] score = { 50, 80, 70, 90, 60};
 		int count = 0;
+		int sum = 0;
 		for(int i = 0; i < score.length; i++) {
 		if (score[i] >= 80) {
 			count++;
-		System.out.println((i + 1) + "番目" + score[i] + "点");
+			sum = sum + score[i];
+		System.out.println((i + 1) + "番目" + score[i] + "点(合格)");
 		}
 		}
 		System.out.println("合格者数" + count + "人");
+		System.out.println("80点以上の合計" + sum + "点");
 		
 	}
 
