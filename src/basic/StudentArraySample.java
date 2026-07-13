d301f4d87ae86c6b10870e4c2fc9713ed5ff257d
nobu-kudo <vstand66@gmail.com>
2026-07-13 11:29:37 +0900

    StudentArraySample.javaの編集

--- src/basic/StudentArraySample.java
+++ src/basic/StudentArraySample.java
@@ -4,13 +4,20 @@ public class StudentArraySample {
 
 	public static void main(String[] args) {
 	
+		int sum = 0;
+		double average;
 		StudentCapsule[] students = new StudentCapsule[3];
 		students[0] = new StudentCapsule("桐生", 85);
 		students[1] = new StudentCapsule("万丈", 35);
 		students[2] = new StudentCapsule("猿渡", 65);
 		for(int i = 0; i < students.length; i++ ) {
 			students[i].showInfo();
+			
+			sum += students[i].getScore();
 		}
+		average = (double) sum / students.length;
+		System.out.println("合計点:" + sum);
+		System.out.printf("平均点:%.2f%n", average);
 	}
 
 }
